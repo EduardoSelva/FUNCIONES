bb28e2bf864358d694e15925b52d67e5470c13f2
Eduardo Selva <Educarselsa@gmail.com>
2026-08-23 20:21:03 -0600

    hola

---
+++ Ejercicio1.py
@@ -0,0 +1,8 @@
+#Eduardo Selva
+print("*** Función: Sumar_valores ***")
+def sumar_valores(x,y):
+    suma = x + y
+    print("La suma de valores es:", suma)
+x = 6
+y = 7
+print(sumar_valores(x,y))
